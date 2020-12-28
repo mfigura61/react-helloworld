2041a7d0c5fcf3b2bd20d4256eb2fa89dc94c4be
mfigura61 <69008862+mfigura61@users.noreply.github.com>
2020-12-28 10:07:45 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello, world and otus from react!!!</div>
+    );
+  }
+}
+
+export default App;
